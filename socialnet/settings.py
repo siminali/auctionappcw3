ddb93d6e3d3e592beadf895b9b54f9a5d93f9bae
Simin <siminali@gmail.com>
2022-12-14 19:57:59 +0000

    vue cors

--- socialnet/settings.py
+++ socialnet/settings.py
@@ -59,6 +59,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'socialnet.urls'
 
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173/",
+
+]
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
